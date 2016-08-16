f328ea95a21866682b3c32a61deef3c50a0e0f52
robertomc2007 <robertomc2007@gmail.com>
2016-08-16 18:28:01 -0300

    Update TEMPV1.ino

--- TEMPERATURA/TEMP_V1/TEMPV1.ino
+++ TEMPERATURA/TEMP_V1/TEMPV1.ino
@@ -2,6 +2,8 @@
 LM35 10mV X °C / 0-100°C
 
 LA REFERENCIA ES 5V y LM35 PUEDE ENTREGAR 1V MAXIMO A 100°C
+
+tempC = (5.0 * lectura * 100.0) / 1024;
 */
 
 float tempC;
@@ -15,8 +17,9 @@ Serial.begin(115200);
 void loop()
 {
 lectura = analogRead(A0);
-//tempC = (5.0 * lectura * 100.0) / 1024;
+
 tempC = map(lectura*1.0, 0.0, 1023.0, 0.0, 100.0);
+
 Serial.println(tempC);
 delay(1000);
 }
