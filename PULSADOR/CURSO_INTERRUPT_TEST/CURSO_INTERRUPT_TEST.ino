7bf7c4cc46b4708a0e74151b8880f790e9c34c80
robertomc2007 <robertomc2007@gmail.com>
2016-10-12 16:49:57 -0300

    Add files via upload

---
+++ PULSADOR/CURSO_INTERRUPT_TEST/CURSO_INTERRUPT_TEST.ino
@@ -0,0 +1,25 @@
+/*
+ * modos:
+LOW to trigger the interrupt whenever the pin is low,
+CHANGE to trigger the interrupt whenever the pin changes value
+RISING to trigger when the pin goes from low to high,
+FALLING for when the pin goes from high to low.
+ */
+
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+}
+
+void blink() {
+  state = !state;
+}
